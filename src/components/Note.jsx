ca3ee75508533c96bfef835ad8b2e385fc6024e5
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-08-20 21:15:31 +0800

    refactor Note.jsx for future readability

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Note = ({ onChecked, id, title, content }) => (
-  <div className='note'>
-    <h1>{title}</h1>
-    <p>{content}</p>
-    <button
-      type='button'
-      onKeyDown={() => {
-        onChecked(id);
-      }}
-      onClick={() => {
-        onChecked(id);
-      }}
-    >
-      Delete
-    </button>
-  </div>
-);
+const Note = ({ onChecked, id, title, content }) => {
+  function handleClick() {
+    onChecked(id);
+  }
+
+  return (
+    <div className='note'>
+      <h1>{title}</h1>
+      <p>{content}</p>
+      <button type='button' onKeyDown={handleClick} onClick={handleClick}>
+        Delete
+      </button>
+    </div>
+  );
+};
 
 Note.propTypes = {
   title: PropTypes.string.isRequired,
