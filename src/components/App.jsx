3521e1494552da0c754780c9d4cd873f8e40f6b3
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-10-30 23:18:03 +0800

    refactor express into its own file and server folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,18 +5,6 @@ import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
 
-const express = require('express');
-const http = require('http');
-const path = require('path');
-
-const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-const port = process.env.PORT || '8080';
-app.set('port', port);
-const server = http.createServer(app);
-// eslint-disable-next-line no-console
-server.listen(port, () => console.log(`Running on localhost:${port}`));
-
 function App() {
   const [items, setItems] = useState([]);
 
